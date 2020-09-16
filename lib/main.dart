4773ef5599b58b1fd0984e0957bbc92ae1dd893a
Ahmet Erdoğan <48730205+ahmeterdgn@users.noreply.github.com>
2020-09-16 22:34:30 +0200

    design start

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        accentColor: Colors.deepOrange,
+        accentColor: Colors.blue[900],
         primaryColor: Colors.deepOrange,
       ),
       // debugShowCheckedModeBanner: false,
